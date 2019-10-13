69477b10036c519770fdb758ada3687880af44c7
paujuanes <54763681+paujuanes@users.noreply.github.com>
2019-10-13 14:04:29 +0200

    New assignment and requirred txt files

---
+++ ex_11_3.py
@@ -0,0 +1,23 @@
+'''
+Exercise 11.3 (Assignment)
+Finding Numbers in a Haystack
+
+In this assignment you will read through and parse a file with text and numbers.
+You will extract all the numbers in the file and compute the sum of the numbers.
+
+*Solution by Pau Juanes, Oct 13th 2019*
+'''
+
+import re
+
+file_r = open('regex_sum_273033.txt')
+numList = list()
+
+for line in file_r:
+    line = line.rstrip()
+    if re.search('\d+', line):
+        lst = re.findall('\d+', line)
+        for num in lst:
+            numList.append(int(num))
+
+print(sum(numList))
